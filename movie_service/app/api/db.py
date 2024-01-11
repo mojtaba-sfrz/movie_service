011af93a2fe296e1374c6214f2fc95d75d385f00
mojtaba-sfrz <msafarzayee@gmail.com>
2024-01-11 20:13:28 +0330

    database created
    initial configuration for sqlalchemy and databases has been done.
    the movie table has been created.

---
+++ movie_service/app/api/db.py
@@ -0,0 +1,23 @@
+from aqlalchemy import (Table, Column, MetaData,
+                         creat_engine, Integer, String, ARRAY)
+
+from dataclasses import Database
+
+import os
+
+DATABASE_URL = os.getenv("MOVIE_ATABASE_URL")
+
+engine = creat_engine(DATABASE_URL)
+metadat = MetaData()
+
+movies = Table(
+    "movies",
+    metadat,
+    Column('id', Integer, primary_key=True),
+    Column('name', String(50)),
+    Column('plot', String(250)),
+    Column('genres', ARRAY(String)),
+    Column('cast_id', ARRAY(Integer)),
+)
+
+database = Database(DATABASE_URL)
--- movie_service/app/main.py
+++ movie_service/app/main.py
@@ -1,6 +1,19 @@
+from app.api.db import database, engine, metadata
 from fastapi import FastAPI
 from app.api.movies import movies
 
+
+metadata.creat_all(engine)
+
 app= FastAPI
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shtdown():
+    await database.disconnect()
 
 app.include_router(movies)
