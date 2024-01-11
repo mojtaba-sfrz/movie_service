12c25f8d61fbcfd2aa2d1f0c9ce8975e83cfcded
mojtaba-sfrz <msafarzayee@gmail.com>
2024-01-11 23:09:36 +0330

    CRUD operation`s queries implemented.

---
+++ movie_service/app/api/db_manager.py
@@ -0,0 +1,53 @@
+from app.api.models import MovieIn, MovieOut, MovieUpdate
+from app.api.db import movies, database
+
+async def add_movie(playload:MovieIn):
+
+    query = movies.insert().values(**playload.dict())
+
+    return await database.execute(query=query)
+
+
+async def get_all_movies():
+
+    query = movies.select()
+
+    return await database.fetch_all(query=query)
+
+
+async def get_movie(id:int):
+
+    query = movies.select(movies.c.id==id)
+
+    return await database.fetch_one(query=query)
+
+
+async def update_movie(id:int, playload:MovieIn):
+
+    query = (
+        movies
+        .update()
+        .where(movies.c.id==id)
+        .values(**playload.dict())
+    )
+
+    return await database.execute(query=query)
+
+async def delete_movie(id:int):
+
+    query = movies.delete().where(movies.c.id==id)
+
+    return await database.execute(query=query)
+
+
+
+
+
+
+
+
+
+
+
+
+
